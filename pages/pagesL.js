f8ae47a1a7b155581e1d91aaf082356a50584d29
multimediav22 <jgth.multimedia@outlook.com>
2024-01-14 15:30:51 -0500

    Arreglos

--- pages/pagesL.js
+++ pages/pagesL.js
@@ -1,23 +1,22 @@
-// pagesL.js
+document.addEventListener("DOMContentLoaded", function() {
+  // Agregar eventos de clic a los botones o enlaces aquí
+  var descargarCVButton = document.getElementById("descargarCV");
+  if (descargarCVButton) {
+      descargarCVButton.addEventListener("click", function(event) {
+          event.preventDefault();
 
- 
-  document.getElementById('descargarCV').addEventListener('click', function(event) {
-   
-    event.preventDefault();
+          // Lógica para descargar el CV
+          var rutaHojaDeVida = "content/Hoja_de_vida_Jairo_Andres_Vergara2024_-_Software9.5N.pdf";
 
- 
-    var rutaHojaDeVida = "content/Hoja_de_vida_Jairo_Andres_Vergara2024_-_Software9.5N.pdf";
+          var linkDeDescarga = document.createElement('a');
+          linkDeDescarga.href = rutaHojaDeVida;
+          linkDeDescarga.download = 'Hoja_de_vida_Jairo_Andres_Vergara2024_-_Software9.5N.pdf';
 
-   
-    var linkDeDescarga = document.createElement('a');
-    linkDeDescarga.href = "content/Hoja_de_vida_Jairo_Andres_Vergara2024_-_Software9.5N.pdf";
-    linkDeDescarga.download = 'Hoja_de_vida_Jairo_Andres_Vergara2024_-_Software9.5N.pdf';
+          document.body.appendChild(linkDeDescarga);
+          linkDeDescarga.click();
 
-  
-    document.body.appendChild(linkDeDescarga);
-    linkDeDescarga.click();
-
-    
-    document.body.removeChild(linkDeDescarga);
-  });
-
+          document.body.removeChild(linkDeDescarga);
+      });
+  }
+  // Agregar más eventos según sea necesario
+});
